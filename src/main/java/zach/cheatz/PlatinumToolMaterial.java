36d47992e99fb07cadd9b6dc96dd67ea1b4bc60e
Zach11111 <zacrut23@gmail.com>
2023-03-30 14:53:23 -0500

    add

---
+++ src/main/java/zach/cheatz/PlatinumToolMaterial.java
@@ -0,0 +1,53 @@
+package zach.cheatz;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.*;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+import net.minecraft.util.registry.Registry;
+
+//Tool Material
+public class PlatinumToolMaterial implements ToolMaterial {
+    public static final PlatinumToolMaterial INSTANCE = new PlatinumToolMaterial();
+
+    @Override
+    public int getDurability() {
+        return 4062;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 12.0F;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return 6.0F;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return 5;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 22;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.BEDROCK);
+    }
+
+
+
+//@Override
+//public void onInitialize() {
+//}
+}
+
